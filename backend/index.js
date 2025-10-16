db6e52c2f134a202d2cb6617f26cc8bbf12b8f3a
SiddharthCA96 <ss6156852@gmail.com>
2025-10-16 13:14:25 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.use("/api/auth", allRoutes);
 // Start server locally only
 // --------------------
 if (process.env.VERCEL !== "1") {
-  const PORT =  3000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
